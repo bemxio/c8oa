1e7b5d9f2498af0181224aacf4026421c4b8ec59
bemxio <bemxiov@protonmail.com>
2024-06-29 22:03:19 +0200

    write the header for the interpreter

---
+++ src/interpreter.hpp
@@ -0,0 +1,20 @@
+#ifndef INTERPRETER_HPP
+#define INTERPRETER_HPP
+
+#include <SDL2/SDL.h>
+#include <cstdint>
+
+class Interpreter {
+    public:
+        Interpreter();
+        ~Interpreter();
+
+        void run();
+
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        SDL_Event event;
+};
+
+#endif
