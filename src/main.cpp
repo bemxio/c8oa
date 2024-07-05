39687874cb262faf37782d2714a52c03578a3a8b
bemxio <bemxiov@protonmail.com>
2024-07-05 17:06:22 +0200

    change debug ROM once again

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,7 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Failed to open file: %s\n", argv[1]); return 1;
     }
 
-    file.seekg(0, file.end);
-    size = file.tellg();
+    file.seekg(0, file.end); size = file.tellg();
     file.seekg(0, file.beg);
 
     Interpreter interpreter;
